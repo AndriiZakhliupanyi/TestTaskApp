e5e7a177a040733783cdf448fa2d944767cc8b35
Andrii Zakhliupanyi <andrii.zakhliupanyi@gmail.com>
2023-02-13 14:03:20 +0200

    Initial Commit

---
+++ TestTaskApp/TestTaskAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestTaskAppApp.swift
+//  TestTaskApp
+//
+//  Created by Andrii Zakhliupanyi on 13.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct TestTaskAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
